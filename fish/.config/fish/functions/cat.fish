47fc1659b7ba69cf941c7cf57c96fdb12aa304ee
Marcel Guzik <mguzik555@gmail.com>
2020-02-19 14:13:37 +0100

    Check if bat exists and what's its name before use

--- fish/.config/fish/functions/cat.fish
+++ fish/.config/fish/functions/cat.fish
@@ -1,4 +1,10 @@
 # Defined in - @ line 1
-function cat --description 'alias cat=batcat'
-	bat $argv;
+function cat --description 'alias cat=batcat|bat|cat'
+	if [ -f /usr/bin/batcat ]
+		batcat $argv;
+	else if [ -f /usr/bin/bat ]
+		bat $argv
+	else
+		cat $argv
+	end
 end
